fc8d4a4d627779faacc8a4015b3b1dfeccf703cd
seherrr <86481287+seherrr@users.noreply.github.com>
2022-02-06 23:41:02 +0300

    Add files via upload

---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace Koleksiyonlar_Soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList dizi = new ArrayList();
+            Console.Write("Bir cümle giriniz: ");
+            string metin=Console.ReadLine();
+            string sesli = "aeıioöuüAEIİOÖUÜ";
+            int sayac = 0;
+            for (int i=0;i<metin.Length;i++)
+            {
+                if (sesli.Contains(metin[i]))
+                {
+                    sayac++;
+                    dizi.Add(metin[i]);
+                }
+
+            }
+
+            Console.WriteLine("Kelime içerisinde toplam " + sayac + " tane sesli harf var.");
+            Console.WriteLine("Bu sesli harfler:");
+            foreach (var item in dizi)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
