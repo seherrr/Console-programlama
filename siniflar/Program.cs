f0e1cbd696a94da68a160fe498befd24efcfa818
seherrr <86481287+seherrr@users.noreply.github.com>
2022-02-07 11:53:14 +0300

    Add files via upload

---
+++ siniflar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace siniflar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisanlar calisan = new Calisanlar();
+            calisan.Ad = "Seher";
+            calisan.Soyad = "Elbasan";
+            calisan.No = 12345678;
+            calisan.Departman = "IT";
+            calisan.CalisanBilgileri();
+            Console.WriteLine("*********");
+            calisan.Ad = "Merve";
+            calisan.Soyad = "Arslan";
+            calisan.No = 87456321;
+            calisan.Departman = "İnsan Kaynakları";
+            calisan.CalisanBilgileri();
+        }
+    }
+    class Calisanlar
+    {
+        public string Ad;
+        public string Soyad;
+        public int No;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışanın Adı : {0}", Ad);
+            Console.WriteLine("Çalışanın Soyadı : {0}", Soyad);
+            Console.WriteLine("Çalışanın Numarası : {0}", No);
+            Console.WriteLine("Çalışanın Departmanı : {0}", Departman);
+
+        }
+
+
+
+    }
+}
